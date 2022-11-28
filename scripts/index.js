ba2da407b5138c2c1992dd4ca8f5819f33d5ca44
Kirill <Kirill.pros@gmail.com>
2022-11-29 03:33:59 +0600

    Исправить после ревью 1

--- scripts/index.js
+++ scripts/index.js
@@ -4,15 +4,19 @@ let popup = page.querySelector('.popup');
 let popupClose = page.querySelector('.popup__close');
 let profileName = page.querySelector('.profile__name');
 let profileJob = page.querySelector('.profile__job');
-let nameInput = page.querySelector('.popup__input-username'); 
-let jobInput = page.querySelector('.popup__input-work');
+let nameInput = page.querySelector('.popup__input_type_name'); 
+let jobInput = page.querySelector('.popup__input_type_job');
 
+// открытие формы редактирования профиля
 function openPopup () {
   popup.classList.add('popup_opened');
+
+  // передача в поля формы значений из профайла
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
+// закрытие формы редактирования профиля
 function closePopup () {
   popup.classList.remove('popup_opened');
 }
@@ -20,8 +24,11 @@ function closePopup () {
 editButton.addEventListener('click', openPopup);
 popupClose.addEventListener('click', closePopup);
 
+// обработчик отправки формы
 function handleFormSubmit (evt) {
-  evt.preventDefault();
+  evt.preventDefault(); 
+  
+  // перезапись значений профайла
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
   closePopup();

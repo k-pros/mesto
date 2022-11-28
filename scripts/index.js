55b8a6276ec438d9d6db9cc06c3ee27b8e1e49b9
Kirill <Kirill.pros@gmail.com>
2022-11-29 01:35:43 +0600

    fix: обработка сохранения формы

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,30 @@
 let page = document.querySelector('.page');
 let editButton = page.querySelector('.profile__edit-btn');
-let addButton = page.querySelector('.profile__add-btn');
 let popup = page.querySelector('.popup');
 let popupClose = page.querySelector('.popup__close');
-let saveButton = page.querySelector('.popup__btn');
 let profileName = page.querySelector('.profile__name');
 let profileJob = page.querySelector('.profile__job');
 let nameInput = page.querySelector('.popup__input-username'); 
 let jobInput = page.querySelector('.popup__input-work');
 
-editButton.addEventListener('click', function () {
+function openPopup () {
   popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
-});
+}
 
-popupClose.addEventListener('click', function () {
+function closePopup () {
   popup.classList.remove('popup_opened');
-});
+}
 
-saveButton.addEventListener('click', function (evt) {
-  evt.preventDefault(); 
+editButton.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
 
+function handleFormSubmit (evt) {
+  evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  popup.classList.remove('popup_opened');
-});
+  closePopup();
+}
+
+popup.addEventListener('submit', handleFormSubmit);
